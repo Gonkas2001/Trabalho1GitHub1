c97abaf0067400d6a7ec681b9d05f49b22c692e1
Gonkas2001 <goncalofernandessilva2001@gmail.com>
2026-05-28 12:42:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 print("Olá, espero que estejas bem!")
-nome=input("Como te chamas?")
-print(f"Espero que aprender a usar GitHub seja fácil para ti, {nome}")
+
+
+def pedir_nome():
+
+    nome = input("Como te chamas? ")
+
+    print(f"Espero que aprender a usar GitHub seja fácil para ti, {nome}")
+
+
+def perguntar():
+
+    while True:
+
+        resposta = input("Tens dúvidas com o GitHub? (s/n) ").lower()
+
+        if resposta in ["s", "sim"]:
+            print("Ooops!")
+
+        elif resposta in ["n", "nao", "não"]:
+            print("Boa!")
+
+        else:
+            print("Resposta inválida.")
+
+        continuar = input("Queres continuar? (s/n) ").lower()
+
+        if continuar not in ["s", "sim"]:
+            print("Adeus!")
+            break
+
+pedir_nome()
+perguntar()
